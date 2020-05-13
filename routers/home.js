7d58880c242bf8866ced9112a31e856fcbef1ff3
Jip Fr <jipfrijlink@gmail.com>
2020-05-13 18:44:36 +0200

    Dynamic blog loading

--- routers/home.js
+++ routers/home.js
@@ -1,14 +1,16 @@
 
-const { getLocale } = require("../util");
+const { getLocale, getPosts } = require("../util");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
 
 	let locale = getLocale("us", "en");
+	let posts = getPosts();
 
 	res.render("home", {
-		locale
+		locale,
+		posts
 	});
 });
 
--- util/index.js
+++ util/index.js
@@ -1,9 +1,49 @@
 
 const fs = require("fs");
 
+const postsPath = "data/posts/";
+
 module.exports.getLocale = (lang, variant) => {
 	let path = `./locales/${lang.toLowerCase()}_${variant.toLowerCase()}.json`;
 	let locale;
 	if(fs.existsSync(path)) locale = JSON.parse(fs.readFileSync(path, "utf-8"));
 	return locale || null;
+}
+
+module.exports.getPosts = () => {
+	let posts = [];
+	let postFileNames = fs.readdirSync(postsPath);
+
+	postFileNames = postFileNames.filter(name => name.includes(".json"));
+
+	for(let fileName of postFileNames) {
+		let path = `${postsPath}/${fileName}`;
+		let file = JSON.parse(fs.readFileSync(path, "utf-8"));
+		
+		// If the post is published, start adding relevant data
+		// and also add to the array
+		if(file.state === "published") {
+			let bodyMd = fs.readFileSync(`${postsPath}/${file.md}`, "utf-8");
+			file.body = {
+				md: bodyMd,
+				short: bodyMd
+				  .split("\n")
+				  .slice(1)
+				  .join("\n")
+				  .trim()
+				  .replace(/\n(\n)?/g, ". ")
+				  .replace(/#/g, "")
+				  .replace(/\.\./g, ".")
+				  .replace(/\!\./g, "!")
+				  .slice(0, 300)	
+			};
+			file.title = bodyMd.split("\n")[0].replace(/#/g, "").trim();
+			posts.push(file);
+		}
+	}
+
+	console.log(posts);
+
+	return posts;
+
 }
