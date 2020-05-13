e5e8ffb110f854542c45a5716c6399bc2de76160
Jip Fr <jipfrijlink@gmail.com>
2020-05-13 20:47:44 +0200

    Improved dark mode, added post page

--- web-server.js
+++ web-server.js
@@ -1,3 +1,5 @@
+const showdown = require("showdown");
+const showdownConverter = new showdown.Converter;
 const routers = require("./routers");
 const express = require("express");
 const handlebars = require("express-handlebars");
@@ -7,7 +9,8 @@ const app = express();
 app.engine("handlebars", handlebars({
 	defaultLayout: "main",
 	helpers: {
-		toSlug: str => encodeURIComponent(str.replace(/ /g, "-"))
+		toSlug: str => encodeURIComponent(str.replace(/ /g, "-")),
+		toHTML: str => showdownConverter.makeHtml(str)
 	}
 }));
 app.set("view engine", "handlebars");
